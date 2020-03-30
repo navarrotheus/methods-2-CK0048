087a35fa11623651c5fce64c01589d95f59a923e
navarrotheus <matheusnavarro@alu.ufc.br>
2020-03-30 19:35:22 -0300

    [~] Showing Images

--- src/Tarefa_01/main.cpp
+++ src/Tarefa_01/main.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
@@ -12,88 +13,94 @@ int main(int argc, char** argv) {
 
   // Lendo imagem e colocando-a em escala de cinza
   Mat image;
-  image = imread(nameImage , CV_LOAD_IMAGE_GRAYSCALE);
+  image = imread(nameImage , CV_LOAD_IMAGE_COLOR);
 
   if(!image.data) {
       cout <<  "Não foi possível abrir ou ler a imagem" << endl ;
       return -1;
   }
+  
+  // Convertendo imagem para escala de cinza
+  Mat grayImage;
+  cvtColor(image, grayImage, CV_BGR2GRAY);
 
   // Criando borda na imagem
-  copyMakeBorder(image, image, 1, 1, 1, 1, BORDER_CONSTANT, 0);
+  copyMakeBorder(grayImage, grayImage, 1, 1, 1, 1, BORDER_CONSTANT, 0);
 
-  // Criando nova imagem
-  Mat newImage = Mat::zeros( image.size(), image.type() );
 
-  // Menu de inicialização
-  int opt;
-  cout << "Selecione a opção desejada" << endl;
-  cout << "1) Box Blur" << endl;
-  cout << "2) Gaussian Blur" << endl;
-  cout << "3) Detector de bordas" << endl;
-  cout << "4) Sharpen Effect" << endl;
-  cout << "5) Embossing Effect" << endl;
-  cin >> opt;
+  // Realizando algoritmos
 
-  while (opt != 1 && opt != 2 && opt != 3 && opt != 4 && opt != 5) {
-    cout << "Opção inválida, entre novamente" << endl;
-    cin >> opt;
+  // Criando nova imagem p/ Box Blur
+  Mat boxBlurImage = Mat::zeros(grayImage.size(), grayImage.type());
+
+  // Box Blur
+  // Como temos uma borda, começamos pelo index 1 e percorremos uma linha e uma coluna a menos
+  for (int i = 1; i < image.rows - 1; i++){
+    for (int j = 1; j < image.cols - 1; j++){
+      boxBlurImage.at<uchar>(i,j) = abs((grayImage.at<uchar>(i+1,j) + grayImage.at<uchar>(i-1,j)
+      + grayImage.at<uchar>(i,j+1) + grayImage.at<uchar>(i,j-1))/4);
+    }
   }
 
-  // Realizando algoritmo de acordo com opção escolhida
-  switch (opt) {
-    case 1:
-      // Como temos uma borda, começamos pelo index 1 e percorremos uma linha e uma coluna a menos
-      for (int i = 1; i < image.rows - 1; i++){
-        for (int j = 1; j < image.cols - 1; j++){
-          newImage.at<uchar>(i,j) = abs((image.at<uchar>(i+1,j) + image.at<uchar>(i-1,j)
-          + image.at<uchar>(i,j+1) + image.at<uchar>(i,j-1))/4);
-        }
-      }
-      break;
-    case 2:
-      for (int i = 1; i < image.rows - 1; i++){
-        for (int j = 1; j < image.cols - 1; j++){
-          newImage.at<uchar>(i,j) = abs((image.at<uchar>(i+1,j-1) + (image.at<uchar>(i+1,j)*2)
-          + image.at<uchar>(i+1,j+1) + (image.at<uchar>(i,j+1)*2) + (image.at<uchar>(i,j)*4)
-          + (image.at<uchar>(i,j-1)*2) + image.at<uchar>(i-1,j-1) + (image.at<uchar>(i-1,j)*2)
-          + image.at<uchar>(i-1,j+1))/16);
-        }
-      }
-      break;
-    case 3:
-      for (int i = 1; i < image.rows - 1; i++){
-        for (int j = 1; j < image.cols - 1; j++){
-          newImage.at<uchar>(i,j) = abs(image.at<uchar>(i,j+1) - image.at<uchar>(i,j-1)
-          + image.at<uchar>(i+1,j) - image.at<uchar>(i-1,j));
-        }
-      }
-      break;
-    case 4:
-      for (int i = 1; i < image.rows - 1; i++){
-        for (int j = 1; j < image.cols - 1; j++){
-          newImage.at<uchar>(i,j) = abs((image.at<uchar>(i,j)*5) - image.at<uchar>(i+1,j)
-          - image.at<uchar>(i-1,j) - image.at<uchar>(i,j+1) - image.at<uchar>(i,j-1));
-        }
-      }
-      break;
-    case 5:
-      for (int i = 1; i < image.rows - 1; i++){
-        for (int j = 1; j < image.cols - 1; j++){
-          newImage.at<uchar>(i,j) = abs(image.at<uchar>(i+1,j-1) + image.at<uchar>(i+1,j)
-          + image.at<uchar>(i,j-1) - image.at<uchar>(i,j+1) - image.at<uchar>(i-1,j)
-          - image.at<uchar>(i-1,j+1) + 128);
-        }
-      }
-      break;
+  // Criando nova imagem p/ Gaussian Blur
+  Mat gaussianBlurImage = Mat::zeros(grayImage.size(), grayImage.type());
+
+  for (int i = 1; i < grayImage.rows - 1; i++){
+    for (int j = 1; j < grayImage.cols - 1; j++){
+      gaussianBlurImage.at<uchar>(i,j) = abs((grayImage.at<uchar>(i+1,j-1) + (grayImage.at<uchar>(i+1,j)*2)
+      + grayImage.at<uchar>(i+1,j+1) + (grayImage.at<uchar>(i,j+1)*2) + (grayImage.at<uchar>(i,j)*4)
+      + (grayImage.at<uchar>(i,j-1)*2) + grayImage.at<uchar>(i-1,j-1) + (grayImage.at<uchar>(i-1,j)*2)
+      + grayImage.at<uchar>(i-1,j+1))/16);
+    }
   }
 
-  // Demonstrando imagem
+  // Criando nova imagem p/ Detector de bordas
+  Mat borderDetectionImage = Mat::zeros(grayImage.size(), grayImage.type());
+
+  for (int i = 1; i < grayImage.rows - 1; i++){
+    for (int j = 1; j < grayImage.cols - 1; j++){
+      borderDetectionImage.at<uchar>(i,j) = abs(grayImage.at<uchar>(i,j+1) - grayImage.at<uchar>(i,j-1)
+      + grayImage.at<uchar>(i+1,j) - grayImage.at<uchar>(i-1,j));
+    }
+  }
+
+  // Criando nova imagem p/ Sharpen Effect
+  Mat sharpenEffectImage = Mat::zeros(grayImage.size(), grayImage.type());
+
+  for (int i = 1; i < grayImage.rows - 1; i++){
+    for (int j = 1; j < grayImage.cols - 1; j++){
+      sharpenEffectImage.at<uchar>(i,j) = abs((grayImage.at<uchar>(i,j)*5) - grayImage.at<uchar>(i+1,j)
+      - grayImage.at<uchar>(i-1,j) - grayImage.at<uchar>(i,j+1) - grayImage.at<uchar>(i,j-1));
+    }
+  }
+
+  // Criando nova imagem p/ Embossing Effect
+  Mat embossingEffectImage = Mat::zeros(grayImage.size(), grayImage.type());
+
+  for (int i = 1; i < grayImage.rows - 1; i++){
+    for (int j = 1; j < grayImage.cols - 1; j++){
+      embossingEffectImage.at<uchar>(i,j) = abs(grayImage.at<uchar>(i+1,j-1) + grayImage.at<uchar>(i+1,j)
+      + grayImage.at<uchar>(i,j-1) - grayImage.at<uchar>(i,j+1) - grayImage.at<uchar>(i-1,j)
+      - grayImage.at<uchar>(i-1,j+1) + 128);
+    }
+  }
+
+  // Demonstrando imagens
   namedWindow( "Imagem original", WINDOW_AUTOSIZE );
-  namedWindow( "Imagem nova", WINDOW_AUTOSIZE );
+  namedWindow( "Escala de cinza", WINDOW_AUTOSIZE );
+  namedWindow( "Box Blur", WINDOW_AUTOSIZE );
+  namedWindow( "Gaussian Blur", WINDOW_AUTOSIZE );
+  namedWindow( "Detector de bordas", WINDOW_AUTOSIZE );
+  namedWindow( "Sharpen Effect", WINDOW_AUTOSIZE );
+  namedWindow( "Embossing Effect", WINDOW_AUTOSIZE );
 
   imshow( "Imagem original", image );
-  imshow( "Imagem nova", newImage );
+  imshow( "Escala de cinza", grayImage );
+  imshow( "Box Blur", boxBlurImage );
+  imshow( "Gaussian Blur", gaussianBlurImage );
+  imshow( "Detector de bordas", borderDetectionImage );
+  imshow( "Sharpen Effect", sharpenEffectImage );
+  imshow( "Embossing Effect", embossingEffectImage );
 
   cout << "Pressione qualquer tecla para interromper a exibição" << endl;
 
