e71cf68c4a3a500b2a4c575a8d2fdab2e79a0ac7
navarrotheus <matheusnavarro@alu.ufc.br>
2020-03-30 14:13:30 -0300

    [+] Box Blur

---
+++ src/Tarefa_01/README.md
@@ -0,0 +1,7 @@
+<a href="http://www.codebind.com/cpp-tutorial/install-opencv-ubuntu-cpp">Instalação OpenCV Ubuntu C++</a>
+
+<a href="https://docs.opencv.org/2.4/doc/tutorials/tutorials.html">Documentação OpenCV</a>
+
+<a href="https://www.opencv-srf.com/p/introduction.html">Documentação OpenCV 2</a>
+
+<a href="https://www.youtube.com/watch?v=waNQ-7ckw0I">Vídeo máscaras</a>
--- src/Tarefa_01/main.cpp
+++ src/Tarefa_01/main.cpp
@@ -1,20 +1,50 @@
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+using namespace cv;
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+  // Lendo imagem e colocando-a em escala de cinza
+  Mat image;
+  image = imread("sample.jpeg" , CV_LOAD_IMAGE_GRAYSCALE);
+
+  if(!image.data) {
+      cout <<  "Could not open or find the image" << endl ;
+      return -1;
+  }
+
+  // Criando nova imagem
+  Mat newImage = Mat::zeros( image.size(), image.type() );
+
+  // Menu de inicialização
   int opt;
   cout << "Selecione a opção desejada" << "\n";
   cout << "1) Box Blur" << "\n";
   cout << "2) Gaussian Blur" << "\n";
   cin >> opt;
 
+  // Realizando algoritmo de acordo com opção escolhida
   switch (opt) {
     case 1:
-      printf("1");
+      for (int i = 0; i < image.rows; i++){
+        for (int j = 0; j < image.cols; j++){
+          newImage.at<uchar>(i, j) = (image.at<uchar>(i-1,j) + image.at<uchar>(i+1, j)
+          + image.at<uchar>(i, j-1) + image.at<uchar>(i+1, j+1))/4;
+        }
+      }
       break;
     case 2:
-      printf("2");
       break;
   }
+
+  // Demonstrando imagem
+  namedWindow( "Original image", WINDOW_AUTOSIZE );
+  namedWindow( "New image", WINDOW_AUTOSIZE );
+
+  imshow( "Original image", image );
+  imshow( "New image", newImage );
+
+  waitKey(0);
+  return 0;
 }
